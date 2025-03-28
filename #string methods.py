cecc969f45f5de30944faa0eff6a762f0680216e
ellen sirleaf <oderosirleaf11@gmail.com>
2025-03-28 23:24:09 +0300

    string methods and type casting

---
+++ #string methods.py
@@ -0,0 +1,21 @@
+#string methods
+name="ellen sirleaf"
+print(len(name))  #prints 13
+print(name.find("i"))  #prints 7
+print(name.capitalize())#prints E llen sirleaf i.e capitalises the first letter 
+print(name.upper())#changes everything to uppercase
+print(name.lower()) #changes everything to lowercase
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("e"))
+print(name.replace("s","k"))
+print(name*6)
+
+
+#type casting --converting the data type of a value to another data type
+x=1
+y=0.4
+z="3"
+print(x)
+print(int(y))
+print(z)
